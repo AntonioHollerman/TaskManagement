8b011094b08e47cda1b7bf9b5fa0c6f1539d9156
Antonio Hollerman <toniohollerprof@gmail.com>
2024-06-05 23:57:31 -0400

    Create getters to return all the rows for each table

--- db_funcs.py
+++ db_funcs.py
@@ -1,12 +1,11 @@
 import sqlite3 as sql
-from collections import namedtuple
+from table_records import *
+from typing import List
+import datetime
 
 # Creates / Connect to database
 conn = sql.connect('TasksManager.db')
 cur = conn.cursor()
-
-# Named tuples that represent rows for each table
-AccountRow = namedtuple("AccountRow", [])
 
 
 def create_tables():
@@ -38,3 +37,62 @@ CREATE TABLE IF NOT EXISTS tasks_completed(
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 );""")
     conn.commit()
+
+
+def get_all_accounts() -> List[AccountRow]:
+    cur.execute("SELECT * FROM accounts")
+
+    accounts: List[AccountRow] = []
+    for acc_id, name in cur.fetchall():
+        accounts.append(AccountRow(
+            int(acc_id),
+            name
+        ))
+    return accounts
+
+
+def get_all_check_lists() -> List[CheckListRow]:
+    cur.execute("SELECT * FROM check_lists")
+
+    check_lists: List[CheckListRow] = []
+    for cl_id, name, items in cur.fetchall():
+        check_lists.append(CheckListRow(
+            int(cl_id),
+            name,
+            items.split(", ")
+        ))
+    return check_lists
+
+
+def get_all_tasks() -> List[TaskRow]:
+    cur.execute("SELECT * FROM tasks")
+
+    tasks: List[TaskRow] = []
+    for task_id, cl_id, name, group_name, bugged in cur.fetchall():
+        tasks.append(TaskRow(
+            int(task_id),
+            int(cl_id),
+            name,
+            group_name,
+            "true" == bugged.lower()
+        ))
+    return tasks
+
+
+def get_all_completed_tasks() -> List[TaskCompletedRow]:
+    cur.execute("SELECT * FROM tasks_completed")
+
+    completed: List[TaskCompletedRow] = []
+    for acc_id, task_id, item_name, date_completed in cur.fetchall():
+        month, day, year = date_completed.split("/")
+        completed.append(TaskCompletedRow(
+            int(acc_id),
+            int(task_id),
+            item_name,
+            datetime.date(
+                int(year),
+                int(month),
+                int(day)
+            )
+        ))
+    return completed
